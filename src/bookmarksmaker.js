3541b5d0606984ca54cec5a8d794519ff273bd3f
Taylor Bradshaw <taylordyoung1@gmail.com>
2019-12-18 09:19:54 -0500

    css syling

--- src/bookmarksmaker.js
+++ src/bookmarksmaker.js
@@ -21,18 +21,18 @@ const generateBookmarkItem = function (item) {
     if (item.rating < store.filter) {
         return "";
     }
-    let htmlString = `<li class="bookmarks js-individual-bookmark" id="${item.id}"><p class="title">${item.title}</p>`;
     if (item.expanded === false) {
-        htmlString += generateStarRating(item.rating) + 
-        `<button class="btn js-delete-button"><i class="far fa-trash-alt"></i></button>
-        <button class="btn js-arrow-button"><i class="fa fa-chevron-down"></i></button>`;
+        let htmlString = `<li class="bookmarks js-individual-bookmark" id="${item.id}"><p class="title">${item.title}</p><section class="starrating">` + generateStarRating(item.rating) + `</section>` +
+        `<section class="bookmarkButtons"><button class="btn js-delete-button"><i class="far fa-trash-alt"></i></button>
+        <button class="btn js-arrow-button"><i class="fa fa-chevron-down"></i></button></section>`;
         return htmlString;
     }
     else {
-        htmlString += `<p>${item.desc}</p>` + generateStarRating(item.rating) +
-        `<button onclick="window.location.href = '${item.url}';">Website</button>
-        <button class="btn js-delete-button"><i class="far fa-trash-alt"></i></button>
-        <button class="btn js-arrow-button"><i class="fa fa-chevron-up"></i></button>
+        let htmlString = `<li class="expanded js-individual-bookmark" id="${item.id}"><section class="expandedRowOne"><p class="title">${item.title}</p><section class="starrating">` + generateStarRating(item.rating) + `</section>` +
+        `<button class="btn js-delete-button"><i class="far fa-trash-alt"></i></button>
+        <button class="btn js-arrow-button"><i class="fa fa-chevron-up"></i></button></section>` +
+        `<section class="expandedRowTwo"><p class="desc">${item.desc}</p>
+        <button class="website" onclick="window.location.href = '${item.url}';">Website</button></section>
         </li>`;
         return htmlString;
     }
@@ -41,7 +41,7 @@ const generateBookmarkItem = function (item) {
 const generateBookmarkString = function (bookmarks) {
     const liItems = bookmarks.map((item) => generateBookmarkItem(item));
     const liTogether = liItems.join('');
-    return `<section class="js-bookmark-list"><ul class="bookmarklist">${liTogether}</ul></section>`
+    return `<section class="js-bookmark-list bookmarkSection"><ul class="bookmarklist">${liTogether}</ul></section>`
 };
 
 const generateButtonString = function () {
@@ -67,20 +67,19 @@ const generateAddForm = function () {
         <label for="url">URL:</label>
         <input type="url" name="url" id="url" required>
         <label for="rating">Rating:</label>
-        <select name="rating" id="rating">
+        <select name="rating" id="rating" class="ratingform">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
             <option value="4">4</option>
             <option value="5">5</option>
         </select>
-        <br><br>
         <label for="bookmarkdescription">Description:</label>
-        <br><br>
         <textarea class="textbox" name="description" id="bookmarkdescription" maxlength="250" placeholder="max 250 characters"></textarea>
-        <br><br>
-        <button type="submit" class="js-add-confirm">Add</button>
-        <button type="reset" class="js-dont-add">Cancel</button>
+        <section class="formButtons">
+        <button type="submit" class="addFormButton js-add-confirm">Add</button>
+        <button type="reset" class="dontAddButton js-dont-add">Cancel</button>
+        </section>
     </form>
 </section>`;
 };
@@ -88,7 +87,7 @@ const generateAddForm = function () {
 const generateErrorMessage = function (message) {
     return `<section class="errorWindow">
                 <p class="errorP">${message}</p>
-                <button id="cancelError" class="js-cancel-error"><i class="far fa-times-circle"></i></button>
+                <button id="cancelError" class="cancelError js-cancel-error"><i class="far fa-times-circle"></i></button>
             </section>`
 }
 
